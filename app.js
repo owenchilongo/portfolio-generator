a5a78e45bcaae9b9ab4131fb9acacac270020672
Owen <ochilongo@gmail.com>
2021-07-03 14:26:31 -0500

    updated template

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./page-template');
+const generatePage = require('./page-template');
 
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
+//    if (err) throw err;
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
+//    console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
 const promptUser = () => {
@@ -142,7 +142,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
   
 
